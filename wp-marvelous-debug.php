10e59c3162aaf0e92bed5df948a168e28dd7fbd5
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2020-03-16 15:03:58 -0300

    Fix author

--- wp-marvelous-debug.php
+++ wp-marvelous-debug.php
@@ -4,7 +4,7 @@
  * Description: Edit the debugging constants from wp-config, view the log file on the Dashboard and more debugging features
  * Version: 1.0.0
  * Author: Thanks to IT
- * Author URI: https://github.com/wp-marvelous-debug
+ * Author URI: https://github.com/wp-marvelous
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wp-marvelous-debug
